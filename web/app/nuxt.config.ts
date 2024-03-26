52b3ebf317d66ecf8b7ecdb0d4c97e18ba809c00
having11 <gatoninja236@gmail.com>
2024-03-26 01:36:06 -0500

    add vuetify

--- web/app/nuxt.config.ts
+++ web/app/nuxt.config.ts
@@ -1,6 +1,27 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
+
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true
+    }
+  },
+  build: {
+    transpile: ['vuetify'],
+  },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }))
+      })
+    },
+    //...
+  ],
   vite: {
     build: {
       target: 'esnext'
@@ -9,7 +30,12 @@ export default defineNuxtConfig({
       supported: {
         'top-level-await': true,
       }
-    }
+    },
+    vue: {
+        template: {
+          transformAssetUrls,
+        },
+    },
   },
   nitro: {
     esbuild: {
